61be992c8fc25079bf379fd76c9611546ce4d35c
PavelVorobyev77 <pascha27_05@mail.ru>
2023-10-25 13:24:32 +0700

    чуток изменил 2.0

--- app/src/main/java/com/example/a20pr/MainActivity.java
+++ app/src/main/java/com/example/a20pr/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Получаем экземпляр базы данных
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        myReff = database.getReference("user555");
+        myReff = database.getReference("user5");
 
         editText1 = findViewById(R.id.editText1);
         editText2 = findViewById(R.id.editText2);

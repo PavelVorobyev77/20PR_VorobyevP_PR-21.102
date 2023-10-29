60d32dc03820ae9648c4fb7f811a81c4a7a6375d
PavelVorobyev77 <pascha27_05@mail.ru>
2023-10-29 20:48:11 +0700

    чуток изменил итог 3.0

--- app/src/main/java/com/example/a20pr/MainActivity.java
+++ app/src/main/java/com/example/a20pr/MainActivity.java
@@ -109,67 +109,67 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public class User {
-        private String firstName;
-        private String lastName;
-        private String phoneNumber;
-        private String email;
-        private String username;
-        private String password;
+        private String afirstName;
+        private String blastName;
+        private String dphoneNumber;
+        private String eemail;
+        private String flogin;
+        private String gpassword;
 
-        public User(String firstName, String lastName, String phoneNumber, String email, String username, String password) {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.phoneNumber = phoneNumber;
-            this.email = email;
-            this.username = username;
-            this.password = password;
+        public User(String afirstName, String blastName, String dphoneNumber, String eemail, String flogin, String gpassword) {
+            this.afirstName = afirstName;
+            this.blastName = blastName;
+            this.dphoneNumber = dphoneNumber;
+            this.eemail = eemail;
+            this.flogin = flogin;
+            this.gpassword = gpassword;
         }
-        public String getFirstName() {
-            return firstName;
-        }
-
-        public void setFirstName(String firstName) {
-            this.firstName = firstName;
+        public String getAFirstName() {
+            return afirstName;
         }
 
-        public String getLastName() {
-            return lastName;
+        public void setAFirstName(String afirstName) {
+            this.afirstName = afirstName;
         }
 
-        public void setLastName(String lastName) {
-            this.lastName = lastName;
+        public String getBLastName() {
+            return blastName;
         }
 
-        public String getPhoneNumber() {
-            return phoneNumber;
+        public void setBLastName(String blastName) {
+            this.blastName = blastName;
         }
 
-        public void setPhoneNumber(String phoneNumber) {
-            this.phoneNumber = phoneNumber;
+        public String getDPhoneNumber() {
+            return dphoneNumber;
         }
 
-        public String getEmail() {
-            return email;
+        public void setDPhoneNumber(String dphoneNumber) {
+            this.dphoneNumber = dphoneNumber;
         }
 
-        public void setEmail(String email) {
-            this.email = email;
+        public String getEEmail() {
+            return eemail;
         }
 
-        public String getUsername() {
-            return username;
+        public void setEEmail(String email) {
+            this.eemail = eemail;
         }
 
-        public void setUsername(String username) {
-            this.username = username;
+        public String getFLogin() {
+            return flogin;
         }
 
-        public String getPassword() {
-            return password;
+        public void setFLogin(String login) {
+            this.flogin = flogin;
         }
 
-        public void setPassword(String password) {
-            this.password = password;
+        public String getGPassword() {
+            return gpassword;
+        }
+
+        public void setGPassword(String password) {
+            this.gpassword = gpassword;
         }
     }
 }
